ae24663569d489e6ee001efada90158226b82e1f
Jesse Mather <jmather@arista.com>
2020-04-08 13:46:48 -0700

    initial import

--- gnmi/entry.py
+++ gnmi/entry.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2020 Arista Networks, Inc.  All rights reserved.
 # Arista Networks, Inc. Confidential and Proprietary.
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
