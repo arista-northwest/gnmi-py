97ea62edd4a1acf98fc196484dd973399012f5b5
Jesse Mather <jmather@arista.com>
2025-04-11 23:38:48 -0700

    typo

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name="gnmi-py",
     packages=find_packages(exclude=["tests"]),
-    install_requires={
+    install_requires=[
         "grpcio>=1.71.0",
         "grpcio-tools==1.71.0",
         "protobuf==5.29.4",
-    },
+    ],
     entry_points={
         'console_scripts': [
             'gnmipy = gnmi:entry.man',
